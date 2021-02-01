15e772f77081b36e4d18a0f1c002bf54341a0a70
Serget Filenko <srgfilenko@gmail.com>
2021-02-01 08:16:45 +0200

    forgot about printing sum after number input

--- SocketServer/Handlers/NumberHandler.cs
+++ SocketServer/Handlers/NumberHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 using SocketServer.Models;
 using SocketServer.Services;
 
@@ -14,7 +17,7 @@ namespace SocketServer.Handlers
 		}
 
 		/// <summary>
-		/// Handles any 'number' command, adds passed number to client storage
+		/// Handles any 'number' command, adds passed number to client storage, and prints current sum for client
 		/// </summary>
 		/// <returns>True if command is a valid number</returns>
 		public bool Handle(ConnectionData connectionData, byte[] commandBytes)
@@ -26,9 +29,23 @@ namespace SocketServer.Handlers
 			{
 				_stateStorage.Clients.TryGetValue(connectionData.ClientIp.ToString(), out var client);
 				client.UserData.Sum += result;
+				Response(connectionData.Socket, $"Sum: {client.UserData.Sum}{Environment.NewLine}");
 			}
 
 			return isNumber;
+		}
+
+
+		private void Response(Socket socket, string response)
+		{
+			var bytes = Encoding.ASCII.GetBytes(response);
+			socket.BeginSend(bytes, 0, bytes.Length, SocketFlags.None, SendCallback, socket);
+		}
+
+		private void SendCallback(IAsyncResult asyncResult)
+		{
+			var socket = (Socket) asyncResult.AsyncState;
+			socket.EndSend(asyncResult);
 		}
 	}
 }
